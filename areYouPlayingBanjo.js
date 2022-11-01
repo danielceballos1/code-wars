9ccf244e99d91dfe6b7ed5bf6f45614396305725
danielceballos1 <danielceballos1@gmail.com>
2022-11-01 14:24:40 +0100

    commit code wars answer

---
+++ areYouPlayingBanjo.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+    //this function takes in a name a checks to see if name starts  with 'r'
+    return name.toLowerCase().startsWith('r')
+      ? name + " plays banjo"
+      : name + " does not play banjo";
+  }
+  //if name starts with 'r' return name + " plays banjo"
+  //else name + " does not play banjo";
+  
