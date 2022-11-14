d2cc69f63d25bc54da5ae47ee6d934d64bbb2466
danielceballos1 <danielceballos1@gmail.com>
2022-11-14 15:17:14 +0100

    commit code wars answer

---
+++ reduceButGrow2.js
@@ -0,0 +1,6 @@
+function grow(x){
+    //reduce function 
+  return x.reduce((acc,curr)=> acc*curr,1)
+  }
+//reduce() function iterates over the elements of the array and multiplies them.
+//1 is the initial value
